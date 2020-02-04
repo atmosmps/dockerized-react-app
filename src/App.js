262f299936e6ba9bc1aff05e7eb50f9677b45187
atmosmps <atmos.mps@gmail.com>
2020-02-04 09:28:29 -0300

    Adicionando arquivos do docker ao projeto.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>. Dockerized React App.
         </p>
         <a
           className="App-link"
